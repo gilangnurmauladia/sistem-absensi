1315354bed8210fe0051abd6499e91d09eaf3600
gilangnurmauladia <jakartanslandsclub@gmail.com>
2026-06-29 15:46:11 +0700

    fix: force global URL scheme to https in production mode

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,17 +3,26 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
         //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        Paginator::useBootstrapFive();
+        // Paksa semua URL menggunakan HTTPS jika di server production
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
